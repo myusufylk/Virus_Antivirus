e9baf8a0bd76c3a9a2078887de5db576a787fffe
Yusuf Yılıkoğlu <yusuf.y0079@icloud.com>
2025-12-23 20:08:37 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Virüs_Antivirüs  // BURASI PROJE ADINLA AYNI OLDU
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
